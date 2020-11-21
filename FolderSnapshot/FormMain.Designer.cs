27e3dc4b18024a4f5e7f96fb92e9c56edd41d30c
Mitradis <mitradis@gmail.com>
2020-11-21 16:47:41 +0300

    Clear Upload

---
+++ FolderSnapshot/FormMain.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace FolderSnapshot
+{
+    partial class FormMain
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonSelectFolder;
+        private System.Windows.Forms.Button buttonSnapshot;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonSelectFolder = new System.Windows.Forms.Button();
+            this.buttonSnapshot = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonSelectFolder
+            // 
+            this.buttonSelectFolder.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSelectFolder.Location = new System.Drawing.Point(12, 12);
+            this.buttonSelectFolder.Name = "buttonSelectFolder";
+            this.buttonSelectFolder.Size = new System.Drawing.Size(112, 36);
+            this.buttonSelectFolder.TabIndex = 1;
+            this.buttonSelectFolder.Text = "Папка";
+            this.buttonSelectFolder.UseVisualStyleBackColor = true;
+            this.buttonSelectFolder.Click += new System.EventHandler(this.buttonSelectFolder_Click);
+            // 
+            // buttonSnapshot
+            // 
+            this.buttonSnapshot.Enabled = false;
+            this.buttonSnapshot.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSnapshot.Location = new System.Drawing.Point(12, 54);
+            this.buttonSnapshot.Name = "buttonSnapshot";
+            this.buttonSnapshot.Size = new System.Drawing.Size(112, 36);
+            this.buttonSnapshot.TabIndex = 3;
+            this.buttonSnapshot.Text = "Снимок";
+            this.buttonSnapshot.UseVisualStyleBackColor = true;
+            this.buttonSnapshot.Click += new System.EventHandler(this.buttonSnapshot_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Enabled = false;
+            this.buttonClear.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClear.Location = new System.Drawing.Point(12, 96);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(112, 36);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Enabled = false;
+            this.buttonDelete.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDelete.Location = new System.Drawing.Point(12, 208);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(112, 36);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(130, 20);
+            this.textBox1.MaxLength = 1024;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(482, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(130, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(482, 196);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 255);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSnapshot);
+            this.Controls.Add(this.buttonSelectFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FolderSnapshot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
+
---
+++ FolderSnapshot/FormMain.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FolderSnapshot
+{
+    public partial class FormMain : Form
+    {
+        static string pathExecuting = Process.GetCurrentProcess().MainModule.FileName;
+        string pathShotsFolder = pathAddSlash(Path.GetDirectoryName(pathExecuting)) + @"FolderSnapshot\";
+        string pathWorkFolder = null;
+        string pathWorkFolderName = null;
+        string selectListItem = null;
+        List<string> snapshotList = new List<string>();
+        List<string> ignoreFilesList = new List<string>();
+
+        public FormMain()
+        {
+            InitializeComponent();
+            if (!Directory.Exists(pathShotsFolder))
+            {
+                try
+                {
+                    Directory.CreateDirectory(pathShotsFolder);
+                }
+                catch
+                {
+                    MessageBox.Show("Нет каталога и не удалось его создать: " + pathShotsFolder + Environment.NewLine + "Дальнейшая работа приложения невозможна.");
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                snapShotsToListBox();
+            }
+            folderBrowserDialog1.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+
+        private void snapShotsToListBox()
+        {
+            listBox1.Items.Clear();
+            foreach (string line in Directory.GetFiles(pathShotsFolder, "*.txt"))
+            {
+                listBox1.Items.Add(Path.GetFileName(line));
+            }
+        }
+
+        private void buttonSelectFolder_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            pathWorkFolder = null;
+            pathWorkFolderName = null;
+            buttonSnapshot.Enabled = false;
+            buttonClear.Enabled = false;
+            DialogResult dialogResult = folderBrowserDialog1.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                pathWorkFolder = pathAddSlash(folderBrowserDialog1.SelectedPath);
+                pathWorkFolderName = new DirectoryInfo(pathWorkFolder).Name;
+                buttonSnapshot.Enabled = true;
+                textBox1.Text = pathWorkFolder;
+            }
+        }
+
+        private void buttonSnapshot_Click(object sender, EventArgs e)
+        {
+            buttonClear.Enabled = false;
+            buttonDelete.Enabled = false;
+            snapshotList.Add("#FolderSnapshot List");
+            searthFiles(pathWorkFolder);
+            searthFolder(pathWorkFolder);
+            string file = pathShotsFolder + pathWorkFolderName + "_" + DateTime.Now.Day + "_" + DateTime.Now.Month + "_" + DateTime.Now.Year + "_" + DateTime.Now.Hour + "_" + DateTime.Now.Minute + "_" + DateTime.Now.Second + ".txt";
+            try
+            {
+                File.WriteAllLines(file, snapshotList, new UTF8Encoding(false));
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось записать файл: " + file);
+            }
+            snapshotList.Clear();
+            snapShotsToListBox();
+        }
+
+        private void searthFolder(string path)
+        {
+            foreach (string text in Directory.GetDirectories(path))
+            {
+                if (!text.Contains("$RECYCLE.BIN") && !text.Contains("System Volume Information"))
+                {
+                    snapshotList.Add(text);
+                    searthFiles(text);
+                    searthFolder(text);
+                }
+            }
+        }
+
+        private void searthFiles(string path)
+        {
+            foreach (string item in Directory.GetFiles(path))
+            {
+                snapshotList.Add(item);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                selectListItem = listBox1.SelectedItem.ToString();
+                string text = selectListItem;
+                for (int i = 0; i < 6; i++)
+                {
+                    int index = text.LastIndexOf("_");
+                    if (index != -1)
+                    {
+                        text = text.Remove(index);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (File.Exists(pathShotsFolder + selectListItem))
+                {
+                    buttonClear.Enabled = pathWorkFolderName == text && File.ReadLines(pathShotsFolder + selectListItem).First() == "#FolderSnapshot List";
+                    buttonDelete.Enabled = true;
+                }
+                else
+                {
+                    buttonClear.Enabled = false;
+                    buttonDelete.Enabled = false;
+                    snapShotsToListBox();
+                }
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Удалить выбранный снимок?", "Подтверждение", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                buttonClear.Enabled = false;
+                buttonDelete.Enabled = false;
+                deleteAny(pathShotsFolder + selectListItem);
+                snapShotsToListBox();
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Удалить все \"чужие\" файлы в выбранном каталоге в соответствии с выбранным снимком?", "Подтверждение", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                if (File.Exists(pathShotsFolder + selectListItem))
+                {
+                    buttonClear.Enabled = false;
+                    ignoreFilesList.AddRange(File.ReadAllLines(pathShotsFolder + selectListItem));
+                    clearFolder(pathWorkFolder);
+                    ignoreFilesList.Clear();
+                    buttonClear.Enabled = true;
+                }
+                else
+                {
+                    buttonClear.Enabled = false;
+                    buttonDelete.Enabled = false;
+                    snapShotsToListBox();
+                }
+            }
+        }
+
+        private void clearFolder(string clearpath)
+        {
+            if (Directory.Exists(clearpath))
+            {
+                foreach (string line in Directory.EnumerateFiles(clearpath))
+                {
+                    if (!ignoreFilesList.Contains(line, StringComparer.OrdinalIgnoreCase))
+                    {
+                        deleteAny(line);
+                    }
+                }
+                foreach (string line in Directory.EnumerateDirectories(clearpath))
+                {
+                    if (!ignoreFilesList.Contains(line, StringComparer.OrdinalIgnoreCase))
+                    {
+                        deleteAny(line);
+                    }
+                    else
+                    {
+                        clearFolder(line);
+                    }
+                }
+            }
+        }
+
+        private void deleteAny(string path)
+        {
+            if (File.Exists(path))
+            {
+                try
+                {
+                    File.Delete(path);
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось удалить файл: " + path);
+                }
+            }
+            else if (Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.Delete(path, recursive: true);
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось удалить каталог: " + path);
+                }
+            }
+        }
+
+        private static string pathAddSlash(string path)
+        {
+            if (path.EndsWith("/") || path.EndsWith(@"\"))
+            {
+                return path;
+            }
+            if (path.Contains("/"))
+            {
+                return path + "/";
+            }
+            if (path.Contains(@"\"))
+            {
+                return path + @"\";
+            }
+            return path;
+        }
+    }
+}
